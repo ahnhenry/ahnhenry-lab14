240a504cb8a91da17a24cd4471149603feede334
Ahn Laurie Henry <ahn.lh2006@gmail.com>
2025-11-10 14:58:41 -0500

    begun implementing skeleton

---
+++ Client.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Client {
+    String address;
+    int port;
+    
+    public void client(String address, int port){
+        this.address = address;
+        this.port = port;
+    }
+
+
+
+}
--- Server.java
+++ Server.java
@@ -1,3 +1,26 @@
+import java.time.*;
+import java.util.ArrayList;
+
 public class Server {
-    
+    int port;
+    static ArrayList<Client> list = new ArrayList<Client>();
+
+    public void server(int port){
+        this.port = port;
+    }
+
+    private void storeClient(Client c){
+        list.add(c);
+    }
+
+    public void disconnect(){
+
+    }
+
+    public int getConnectedTimes(){
+        LocalDateTime time ;
+        
+
+        return;
+    }
 }
